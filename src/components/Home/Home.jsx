e4cf6c0961608a1b99dd1baf8f48bdc625482243
Rubin Imeri <rubinimeri6@gmail.com>
2024-09-08 12:44:44 +0200

    Styling and jsx changes for Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,16 @@ function Home() {
     return(
         <main className={styles.main}>
             <h1>{'Beauty is in the eye of the beholder'.toUpperCase()}</h1>
-            <Link to="/shop">Shop</Link>
+            <div className={styles.flex}>
+                <div className={styles.heroImg}></div>
+                <section className={styles.section}>
+                    <h2>LOREM IPSUM, DOLOR SIT AMET</h2>
+                    <p className={styles.para}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quos quidem ipsam eum, consequuntur officia inventore ducimus doloribus eligendi tenetur unde deserunt, optio pariatur voluptates dolores architecto sunt distinctio. Dignissimos.
+                    </p>
+                    <Link to="/shop">SHOP</Link>
+                </section>
+            </div>
         </main>
     )
 }
