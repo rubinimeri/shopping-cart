d651a2c42a472f95b4d66ea75751c172b63f2ebe
Rubin Imeri <rubinimeri6@gmail.com>
2024-09-08 21:09:33 +0200

    Add example data

---
+++ src/utils.js
@@ -0,0 +1,8 @@
+const exampleData = {
+    images: ['https://i.imgur.com/cHddUCu.jpeg'],
+    title: 'Classic Heather Gray Hoodie',
+    description: 'Stay comfortable and stylish with our Classic High-Waisted Athletic Shorts. Designed for optimal movement and versatility, these shorts are a must-have for your workout wardrobe. Featuring a figure-flattering high waist, breathable fabric, and a secure fit that ensures they stay in place during any activity, these shorts are perfect for the gym, running, or even just casual wear.',
+    price: 70,
+}
+
+export default exampleData
