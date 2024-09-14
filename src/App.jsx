0fe2eb9afb0c068801f3b0891d0b28b556c232e6
Rubin Imeri <rubinimeri6@gmail.com>
2024-09-14 20:09:24 +0200

    Update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "./components/Navbar/Navbar"
+import Cart from "./components/Cart/Cart"
 
 import './styles/App.css'
+import { useState } from "react"
 
 function App() {
+  const [cart, setCart] = useState([]);
+  const [cartVisibility, setCartVisibilty] = useState(false);
+
+  function handleCartVisibilityChange(e) {
+    e.preventDefault();
+    setCartVisibilty(!cartVisibility)
+  }
+
   return (
     <>
-      <Navbar />
-      <Outlet />
+      <div
+      style={{display: cartVisibility ? 'block' : 'none'}}
+      className="overlay"></div>
+      <Navbar
+      cartCount={cart.length}
+      handleCartVisibilityChange={handleCartVisibilityChange} />
+      <Outlet context={[cart, setCart]} />
+      <Cart
+      cart={cart}
+      setCart={setCart}
+      handleCartVisibilityChange={handleCartVisibilityChange}
+      cartVisibility={cartVisibility} />
     </>
   )
 }
