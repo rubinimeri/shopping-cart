c56a9109e77e624ba398ef6f85363eeed8485558
Rubin Imeri <rubinimeri6@gmail.com>
2024-09-14 20:09:14 +0200

    Add routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,16 @@
 import App from "./App";
 import Home from "./components/Home/Home";
+import Shop from "./components/Shop/Shop";
+import ProductPage from "./components/ProductPage/ProductPage";
 
 const routes = [
     {
         path: "/",
         element: <App />,
         children: [
-            { index: true, element: <Home /> }
+            { index: true, element: <Home /> },
+            { path: 'shop', element: <Shop /> },
+            { path: 'product/:productId', element: <ProductPage /> }
         ]
     }
 ]
