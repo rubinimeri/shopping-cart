05ab81eb8ea4240415f06e5c15b5d7d0d89ac86a
Rubin Imeri <rubinimeri6@gmail.com>
2024-09-14 20:07:50 +0200

    Create ProductCard component

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import styles from "./ProductCard.module.css";
+
+function ProductCard({ image, price, title, description, id, handleAddToCart}) {
+
+  return (
+    <Link 
+    id={id} 
+    className={styles.productLink} 
+    to={`/product/${id}`}>
+      <div>
+        <h3 className={styles.title}>{title.toUpperCase()}</h3>
+      </div>
+      <img src={image} alt={description} className={styles.image} />
+      <button onClick={handleAddToCart}>ADD - {price}$</button>
+    </Link>
+  );
+}
+
+ProductCard.propTypes = {
+  image: PropTypes.PropTypes.string,
+  price: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  title: PropTypes.string,
+  description: PropTypes.string,
+  id: PropTypes.number,
+  handleAddToCart: PropTypes.func
+};
+
+export default ProductCard;
